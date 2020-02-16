d0334d5e775cf3569c0369a2cab7e3cd026289ab
gzh <943608716@qq.com>
2020-02-16 11:23:50 +0800

    add comment

--- rpckids/demo/DemoServer.java
+++ rpckids/demo/DemoServer.java
@@ -11,9 +11,12 @@ import rpckids.server.RPCServer;
 public class DemoServer {
 
 	public static void main(String[] args) {
+		//服务器监听端口
 		RPCServer server = new RPCServer("localhost", 8888, 2, 16);  //监听客户端的端口
+		//注册服务
 		server.service("fib", Integer.class, new FibRequestHandler()).service("exp", ExpRequest.class,
 				new ExpRequestHandler());//注册服务
+		//启动服务器
 		server.start();
 	}
 
